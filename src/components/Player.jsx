05c7fa7b481b5f66b67a9be91655d6af61126734
Hung Jin Chong <hungjinchong@Outlook.com>
2021-02-05 15:23:06 +0000

    merge drita

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -35,16 +35,26 @@ const mapDispatchToProps = (dispatch) => ({
 
 class Player extends React.Component {
   state = {
-    img: 'https://e-cdns-images.dzcdn.net/images/artist/0b17b99897d17ceb7027ed57cdbb7044/56x56-000000-80-0-0.jpg',
-    song: 'Bohemian Rhapsody',
-    artist: 'Queen'
-  }
-  componentDidUpdate(prevProps){
-    console.log('player props:::::::::::::::::::::prevProps:', prevProps.newPlaying, ' newProps:', this.props.newPlaying)
-    if(prevProps.newPlaying.id !== this.props.newPlaying.id){
-      this.setState({img: this.props.newPlaying.artist.picture_small ? this.props.newPlaying.artist.picture_small : 'http://placehold.it/60x60',
-                    song: '',
-                    artist: this.props.newPlaying.artist.name })
+    img:
+      "https://e-cdns-images.dzcdn.net/images/artist/0b17b99897d17ceb7027ed57cdbb7044/56x56-000000-80-0-0.jpg",
+    song: "Bohemian Rhapsody",
+    artist: "Queen",
+  };
+  componentDidUpdate(prevProps) {
+    console.log(
+      "player props:::::::::::::::::::::prevProps:",
+      prevProps.nowPlaying,
+      " newProps:",
+      this.props.nowPlaying
+    );
+    if (prevProps.nowPlaying.id !== this.props.nowPlaying.id) {
+      this.setState({
+        img: this.props.nowPlaying.artist.picture_small
+          ? this.props.nowPlaying.artist.picture_small
+          : "http://placehold.it/60x60",
+        song: "",
+        artist: this.props.nowPlaying.artist.name,
+      });
     }
     if (prevProps !== this.props)
       this.setState({ playing: this.props.nowPlaying.id }, () =>
@@ -56,26 +66,39 @@ class Player extends React.Component {
       <>
         <section
           className="player d-flex justify-content-between"
-          style={{ width: "100%", position: "fixed" }}>
+          style={{ width: "100%", position: "fixed" }}
+        >
+          <div className="player-albumart d-flex align-items-center justify-content-start">
+            <div className="nowplaying-albumart mx-3">
+              <img
+                src={
+                  this.props.nowPlaying.cover_small
+                    ? this.props.nowPlaying.cover_small
+                    : this.state.img
+                }
+                style={{ width: "64px", height: "64px" }}
+              />
+            </div>
+            <div className="d-none d-sm-flex flex-column text-left mr-4">
+              <div className="nowplaying-title">
+                {this.props.nowPlaying.title_short
+                  ? this.props.nowPlaying.title_short
+                  : this.state.song}
+              </div>
+              <div className="nowplaying-artist">
+                {this.props.nowPlaying.name}
+              </div>
+            </div>
+            <div className="d-none d-lg-flex loved-track mr-3">
+              <FontAwesomeIcon icon={faHeart} />
+            </div>
+            <FontAwesomeIcon
+              icon={faChalkboard}
+              className="d-none d-lg-block"
+            />
+          </div>
 
-        <div
-          className="player-albumart d-flex align-items-center justify-content-start">
-          <div className="nowplaying-albumart mx-3">
-            <img src={this.props.newPlaying.cover_small ? this.props.newPlaying.cover_small : this.state.img}
-             style={{width: '64px', height: '64px'}}/>
-          </div>
-          <div className="d-none d-sm-flex flex-column text-left mr-4">
-            <div className="nowplaying-title">{this.props.newPlaying.title_short ? this.props.newPlaying.title_short : this.state.song}</div>
-            <div className="nowplaying-artist">{this.props.newPlaying.name}</div>
-          </div>
-          <div className="d-none d-lg-flex loved-track mr-3">
-            <FontAwesomeIcon icon={faHeart} />
-          </div>
-          <FontAwesomeIcon icon={faChalkboard} className="d-none d-lg-block"/>
-        </div>
-  
-        <div className="d-flex flex-column py-2 my-1 flex-grow-1">
-          </div>
+          <div className="d-flex flex-column py-2 my-1 flex-grow-1"></div>
           {/*******************/}
           <div className="d-flex flex-column py-2 my-1 flex-grow-1">
             <div className="player-btn d-flex align-items-center justify-content-center player-controller my-1 py-1">

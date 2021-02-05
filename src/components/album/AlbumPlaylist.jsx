3ba4bf19b2cfc49043378d8f8906af52f623c1e2
Hung Jin Chong <hungjinchong@Outlook.com>
2021-02-05 16:28:05 +0000

    fixed some stuff

--- src/components/album/AlbumPlaylist.jsx
+++ src/components/album/AlbumPlaylist.jsx
@@ -26,7 +26,7 @@ class AlbumPlaylist extends Component {
     loading: "true",
     colors: [],
     show: false,
-    selectedPlaylist: this.props.playlists[0].name,
+    selectedPlaylist: null,
     selectedSong: "",
   };
 
@@ -62,6 +62,9 @@ class AlbumPlaylist extends Component {
 
   componentDidMount = () => {
     this.fetchAlbum(this.props.albumId);
+    if (this.props.playlists.length > 0) {
+      this.setState({ selectedPlaylist: this.props.playlists[0].name });
+    }
   };
 
   fetchAlbum = (id) => {
@@ -207,7 +210,8 @@ class AlbumPlaylist extends Component {
                       <td>
                         <AiOutlinePlusSquare
                           onClick={() => this.handleAddToPlaylist(track.id)}
-                          style={{'marginTop': '1em'}}/>
+                          style={{ marginTop: "1em" }}
+                        />
                       </td>
                       <td style={{ verticalAlign: "middle" }}>
                         <p className="group">{track.rank}</p>

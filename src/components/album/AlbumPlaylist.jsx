11b0a8117551b0c3f36de6ad7488ad40db430ec5
Lidia Kovac <lidia.kovac1998@gmail.com>
2021-02-05 15:33:49 +0100

    Revert "fixes image display for 'play from playlists'"
    This reverts commit 66954a960b8414952e038d027206b12293c1e1f3.

--- src/components/album/AlbumPlaylist.jsx
+++ src/components/album/AlbumPlaylist.jsx
@@ -144,7 +144,7 @@ class AlbumPlaylist extends Component {
                         {key+1}{" "}
                       </span>
                       <BsFillPlayFill
-                        onClick = { () => this.handlePlay({...track, ...this.state.album})}
+                        onClick = { () => this.handlePlay(track)}
                         className="track-play play-track-btn"
                       />
                     </th>
